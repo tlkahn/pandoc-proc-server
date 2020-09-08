abf804fab7e719454f2106f385f23928d6af4541
toeinriver <toeinriver@gmail.com>
2020-09-08 11:00:30 +0800

    full page support

--- index.py
+++ index.py
@@ -2,16 +2,19 @@ import web
 import json
 import pypandoc
 import os
+import requests
+from readability import Document
 
 os.environ.setdefault('PYPANDOC_PANDOC', '/usr/local/bin/pandoc')
 
 urls = (
     '/', 'index',
+    '/fullpage', 'fullpage'
 )
 
 class index:
     def POST(self):
-        html = (web.data().decode('utf8'))
+        html = web.data().decode('utf8')
         # print(f"post successful with {params}")
         # param_dict = json.loads(params)
         # html = param_dict['html']
@@ -25,6 +28,20 @@ class index:
 
         web.header('Content-Type', 'application/text')
         # return json.dumps(res)
+        return res
+
+class fullpage:
+    def POST(self):
+        url = web.data().decode('utf8')
+        response = requests.get(url)
+        doc = Document(response.text)
+        summary = doc.summary()
+        try:
+            output = pypandoc.convert_text(summary, 'org', format='html')
+            res = output
+        except:
+            res = ''
+        web.header('Content-Type', 'application/text')
         return res
 
 if __name__ == "__main__":
