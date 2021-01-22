d35d88113919eb063fdc80d5c930fab4262ec1b9
Josh Kwok <toeinriver@Joshs-MacBook-Air.local>
2021-01-22 16:28:43 +0800

    kinda worked on JSON

--- index.py
+++ index.py
@@ -1,10 +1,9 @@
+import re
 import pypandoc
 import os
-from readability import Document
-import re
+from flask import Flask, request, Response, send_file, make_response
 import gist
-from flask import Flask
-from flask import request, jsonify, Response
+import hashlib
 
 os.environ.setdefault('PYPANDOC_PANDOC', '/usr/bin/pandoc')
 
@@ -51,7 +50,6 @@ def clean(txt, href=''):
         domain = get_domain(href)
     else:
         domain = ''
-
     if len(txt) > 0:
         rm_patterns = [r'<<.*>>', r'\\\\']
         rp_patterns = [
@@ -72,19 +70,16 @@ def clean(txt, href=''):
 
         for p in rm_patterns:
             txt = re.sub(p, r'', txt)
-        for i, p in enumerate(rp_patterns):
+        for _, p in enumerate(rp_patterns):
             txt = re.sub(p[0], p[1], txt)
 
-        save_to_file(txt)
         return txt
 
 
-def save_to_file(content, name='untitled.org', dir='./'):
+def save_to_file(content, name='untitled.org', file_dir='./'):
     try:
-        f = open(dir+name, "a")
+        f = open(file_dir + name, "w")
         f.write(content)
-    except Exception as e:
-        raise('error when writing to file', e)
     finally:
         f.close()
 
@@ -94,36 +89,66 @@ def expand_gist(match_obj):
     return '<pre>{}</pre>'.format(gist.get_content(_id))
 
 
+def get_md5_checksum(input_str):
+
+    return hashlib.md5(input_str.encode('utf-8')).hexdigest()
+
+
 def get_domain(url):
     res = re.match(r'https?://([^/]+?)/.*', url).group(1)
     return res
 
 
 def capture(input_str):
-    input_str = re.sub(r'<div gistlink=\"https://gist.github.com/(.+?)/(.+?)\.js\">', expand_gist, input_str)
+    input_str = re.sub(r'<div gistlink=\"https://gist.github.com/(.+?)/(.+?)\.js\">',
+        expand_gist,
+        input_str)
     try:
-        output = pypandoc.convert_text(input_str, 'org', format='html', extra_args=['--wrap=none'])
-        res = output
+        res = pypandoc.convert_text(input_str, 'org', format='html', extra_args=['--wrap=none'])
     except:
         res = ''
     return res
 
+
+def validate_sig(input_str, sig):
+    return get_md5_checksum(input_str) == sig
+
+
+def safe_str(s):
+    try:
+        return str(s)
+    except UnicodeEncodeError:
+        return s.encode('ascii', 'ignore').decode('ascii')
+    return ""
+
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST'])
 def home():
-  if request.method == 'POST':
-    raw_input = request.form.get('content')
-    # input_text = re.match(r'([^\n]+?)\n(.*)', raw_input)
-    # href = input_text.group(1)
-    html = raw_input
-    result = clean(capture(html), '')
-    print('result: {}'.format(result))
-    r = Response(response=result, status=200)
-    r.headers['Access-Control-Allow-Origin'] = '*'
-    r.headers['Access-Control-Allow-Credentials'] = 'true'
-    r.headers['Content-Type'] = 'application/text; charset=utf-8'
-    
-    return r
+    if request.method == 'POST':
+        html = safe_str(request.json['html'])
+        sig = request.json['sig']
+        href = request.json['url']
+        print('html: {}', html)
+        if validate_sig(href, sig):
+            # file_name = 'untitled.org'
+            # file_dir = './'
+            result = clean(capture(html), href)
+            # print('result: {}'.format(result))
+            # save_to_file(result, file_name, file_dir)
+            r = Response(response=result, status=200)
+            r.headers['Access-Control-Allow-Origin'] = '*'
+            r.headers['Access-Control-Allow-Credentials'] = 'true'
+            r.headers['Content-Type'] = 'application/text; charset=utf-8'
+            # response = make_response(send_file(file_full_path))
+            # response.headers["Content-Disposition"] = \
+            #     "attachment; " \
+            #     "filename={};".format(file_full_path)
+            return r
+    else:
+        return Response(response='ok', status=200)
 
-app.run(host = '0.0.0.0')
+
+app.run(host='0.0.0.0')
